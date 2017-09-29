51d1a60e2691aa47d5fe7b427d34a91e46000a2e
Marten Veldthuis <marten@veldthuis.com>
2017-09-29 11:55:07 +0200

    Add subject urls

--- lib/comms/zoo_stream.ex
+++ lib/comms/zoo_stream.ex
@@ -5,7 +5,7 @@ defmodule ZooStream do
     end)
   end
 
-  def handle("panoptes", "classification", data, _linked) do
+  def handle("panoptes", "classification", data, linked) do
     project_id = data["links"]["project"]
 
     message = %{
@@ -13,15 +13,23 @@ defmodule ZooStream do
       project_id: data["links"]["project"],
       workflow_id: data["links"]["workflow"],
       user_id: data["links"]["user"],
-      subject_ids: [], #subject_ids,
-      subject_urls: [], #subject_urls,
+      subject_ids: data["links"]["subjects"],
+      subject_urls: subject_urls(data["links"]["subjects"], linked["subjects"]),
       geo: nil #Geo.locate(data["user_ip"])
     }
 
-    CommsWeb.Endpoint.broadcast("projects:#{project_id}", "classification", message)
+    CommsWeb.Endpoint.broadcast("project:#{project_id}", "classification", message)
   end
 
   def handle(_, _, _, _) do
     nil
+  end
+
+  defp subject_urls(ids, subjects) do
+    ids = MapSet.new(ids)
+
+    subjects
+    |> Enum.filter(fn subject -> MapSet.member?(ids, subject["id"]) end)
+    |> Enum.flat_map(fn subject -> subject["locations"] end)
   end
 end
